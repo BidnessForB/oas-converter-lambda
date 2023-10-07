f44c05a6c2add9f4e4ba2a25d28e89e3b22b7edf
Bryan Cross <bryanrkcross@gmail.com>
2023-10-07 12:39:22 -0500

    yaml working

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 const AWS = require('aws-sdk');
 var converter = require('api-spec-converter');
+const query = require('querystring');
 
 exports.handler = async (event) => {
-  
   try {
+    
+    console.log(event);
     const swagger2 = JSON.parse(event.body);
+    
+    
   var oas30 = {};
   oas30 = await converter.convert({
     from: 'swagger_2',
