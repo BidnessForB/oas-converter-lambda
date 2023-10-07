9570f6eb17c9c561a668166cae0ccfc3a3f807dd
Bryan Cross <bryanrkcross@gmail.com>
2023-10-07 14:39:02 -0500

    Works for JSON and YAML

--- index.js
+++ index.js
@@ -1,21 +1,97 @@
 // lambda.js
 
 const AWS = require('aws-sdk');
-var converter = require('api-spec-converter');
 const query = require('querystring');
+var converter = require('api-spec-converter');
+var jsyaml = require('js-yaml');
+var fs = require('fs');
 
 exports.handler = async (event) => {
   try {
     
-    console.log(event);
-    const swagger2 = JSON.parse(event.body);
+//    console.log(event);
+
+console.log("Here: 1");
+
+    const supportedAPITypes = [
+    "swagger_1"
+    ,"swagger_2"
+    ,"openapi_3"
+    ,"api_blueprint"
+    ,"io_docs"
+    ,"google"
+    ,"raml"
+    ,"wadl"
+  ]
+  console.log("Here: 2");
+  const supportFileFormats =[
+    "yaml"
+    ,"yml"
+    ,"json"
+  ]
+  console.log("Here: 3", event.rawQueryString);
+  console.log("TYpeof querystring: ", (typeof event.rawQueryString));
+  const rawParams = new URLSearchParams(event.rawQueryString);
+
+  // Get all values.
+  const params = Object.fromEntries(rawParams);
+      
+      console.log("Here: 3.5", params);
+    if(typeof params === 'undefined' || params == null) {
+      console.log("Here: 4");
+      params = {};
+      params.outputFileFormat = 'yaml'
+      params.inputFileFormat = 'json'
+      params.inputAPIType = 'swagger_2'
+      params.outputAPIType = 'openapi_3'
+    }
     
+    else {
+      console.log("Here: 4");
+      params.outputFileFormat = Object.hasOwnProperty.bind(params)("outputFileFormat") ? params.outputFileFormat : 'yaml';
+      params.inputFileFormat = Object.hasOwnProperty.bind(params)("inputFileFormat") ? params.inputFileFormat : 'json';
+      params.inputAPIType = Object.hasOwnProperty.bind(params)("inputAPIType") ? params.inputAPIType : 'swagger_2';
+      params.outputAPIType = Object.hasOwnProperty.bind(params)("outputAPIType") ? params.outputAPIType : 'swagger_2';
+    }
+    if(!supportedAPITypes.includes(params.inputAPIType)) {
+      throw new Error("Unsupported input API type: ", params.inputAPIType);
+    }
+    if(!supportedAPITypes.includes(params.outputAPIType)) {
+      throw new Error("Unsupported output API type: " + params.outputAPIType);
+    }
+    if(!supportFileFormats.includes(params.inputFileFormat)) {
+      throw new Error("Unsupported input file format: ", params.inputFileFormat);
+    }
+    if(!supportFileFormats.includes(params.outputFileFormat)) {
+      throw new Error("Unsupported output file format: ", params.outputFileFormat);
+    }
+    console.log("Here: 5");
+    console.log("Params: ")
+    console.log(params);
+    var b64 = JSON.parse(event.body).b64
+    if(params.inputFileFormat == 'yaml' || params.inputFileFormat == 'yml') {
+      console.log("Transforming YAML")
+      console.log("b64", JSON.parse(event.body).b64);
+      apispec = Buffer.from(b64,'base64').toString('utf-8');
+      //var data = Buffer.from(b64Yaml,'base64').toString('utf-8');
+      var yamlData = jsyaml.load(apispec);
+      apispec = JSON.parse(JSON.stringify((yamlData)));
+      console.log("Transforming YAML COMPLETE")
+    }
+    else {
+      console.log("Here: 6");
+      apispec = JSON.parse(Buffer.from(b64,'base64').toString('utf-8'));
+    }
+
+    console.log("Transformed API Spec");
+    console.log(apispec);
+    console.log("Here 7");
     
   var oas30 = {};
   oas30 = await converter.convert({
-    from: 'swagger_2',
-    to: 'openapi_3',
-    source: swagger2,
+    from: params.inputAPIType,
+      to: params.outputAPIType,
+    source: apispec,
   });
   }
   catch(error) {
